1956d64f0989dcf1e6ecc5525d650f5e18801faf
user1 <>
2018-05-27 10:02:00 +0200

    {/src/main/java/com/myspace/washit/CreateCustomerTaskHandler.java}

--- src/main/java/com/myspace/washit/CreateCustomerTaskHandler.java
+++ src/main/java/com/myspace/washit/CreateCustomerTaskHandler.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
 import javax.json.Json;
+import javax.json.JsonObject;
 import java.net.URL;
 import java.net.URLConnection;
 import javax.net.ssl.HttpsURLConnection;

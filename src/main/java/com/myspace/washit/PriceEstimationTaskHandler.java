22dea2a6e4dd1c082794e11cf537bc1b1f56cc34
user1 <>
2018-05-28 17:17:52 +0200

    {/src/main/java/com/myspace/washit/PriceEstimationTaskHandler.java}

--- src/main/java/com/myspace/washit/PriceEstimationTaskHandler.java
+++ src/main/java/com/myspace/washit/PriceEstimationTaskHandler.java
@@ -19,7 +19,12 @@ public class PriceEstimationTaskHandler implements java.io.Serializable, WorkIte
         //Order order = (Order) workItem.getParameter("Order");
         Customer customer = (Customer) workItem.getParameter("Order");
         
-        System.out.println("DATA OK!!!: " + customer);
+        System.out.println("DATA OK!!!: " + customer.getFirstName());
+        System.out.println("DATA OK!!!: " + customer.getLastName());
+        System.out.println("DATA OK!!!: " + customer.getEmail());
+        System.out.println("DATA OK!!!: " + customer.getPassword());
+        System.out.println("DATA OK!!!: " + customer.getAddredd());
+        System.out.println("DATA OK!!!: " + customer.getActivated());
         
         // Notify manager that work item has been completed
         manager.completeWorkItem(workItem.getId(), new HashMap<String,Object>());

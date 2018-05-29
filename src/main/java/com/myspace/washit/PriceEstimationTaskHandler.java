047ec51a0dd1da46e960c26fcefa6bb0ccfe1ae6
user1 <>
2018-05-29 15:41:29 +0200

    {/src/main/java/com/myspace/washit/PriceEstimationTaskHandler.java}

--- src/main/java/com/myspace/washit/PriceEstimationTaskHandler.java
+++ src/main/java/com/myspace/washit/PriceEstimationTaskHandler.java
@@ -4,6 +4,19 @@ import com.myspace.washit.Order;
 import java.util.Map;
 import java.util.HashMap;
 
+//import javax.json.Json;
+//import javax.json.JsonObject;
+//import javax.json.JsonReader;
+//import java.net.URL;
+//import java.io.InputStreamReader;
+//import java.io.BufferedReader;
+//import java.io.StringReader;
+//import java.net.HttpURLConnection;
+//import java.util.Date;
+//import java.text.SimpleDateFormat;
+//import java.text.DateFormat;
+//import java.util.Locale;
+
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
@@ -29,8 +42,49 @@ public class PriceEstimationTaskHandler implements java.io.Serializable, WorkIte
         System.out.println("LAUNDRY PROGRAM?: " + order.getLaundryProgram());
         System.out.println("IRONING?: " + order.getIroning());
         System.out.println("ADD CLOTHES HANGERS?: " + order.getAddClothesHangers());
-        System.out.println("PICK UP DATE?: " + order.getPickUpDate());
-        System.out.println("DELIVERY DATE?: " + order.getDeliveryDate());
+        
+        // Get price list
+        String url = "http://www.mocky.io/v2/5b0d551531000058009d5632";
+        Date availableDate = null;
+        
+        try {
+            HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
+            
+            // Header
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+            // Send
+            con.setDoOutput(true);
+            con.setInstanceFollowRedirects(false);
+            con.connect();
+            
+            // Response
+            BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+	    	String inputLine;
+		    StringBuffer response = new StringBuffer();
+
+		    while ((inputLine = in.readLine()) != null) {
+			    response.append(inputLine);
+		    }
+		    in.close();
+		    
+		    JsonReader jsonReader = Json.createReader(new StringReader(response.toString()));
+            JsonObject object = jsonReader.readObject();
+            jsonReader.close();
+            String dateString = object.getString("localDate");
+            
+            // Response code
+            int responseCode = con.getResponseCode();
+		    System.out.println("\nSending 'GET' request to URL : " + url);
+		    System.out.println("Response Code : " + responseCode);
+		    
+        }   catch (Exception e) {
+            e.printStackTrace();
+        }
         
         String deposit = "10000";
         
@@ -43,3 +97,34 @@ public class PriceEstimationTaskHandler implements java.io.Serializable, WorkIte
     
     public void	abortWorkItem(WorkItem workItem, WorkItemManager manager) {}
 }
+
+
+
+
+
+
+    public void	executeWorkItem(WorkItem workItem, WorkItemManager manager) 
+    {
+        // Extract parameters
+        Order order = (Order) workItem.getParameter("Order");
+        Date pickupDate = order.getPickUpDate();
+        Date deliveryDate = order.getDeliveryDate();
+        
+        
+
+        if (pickupDate.compareTo(availableDate) <= 0) 
+        {
+            System.out.println("SELECTED DATE IS NOT AVAILABLE");
+            throw new RuntimeException("The selected date is not available.");
+        }
+        
+        // Check if delivery is later than pick up
+        if (pickupDate.compareTo(deliveryDate) >= 0) 
+        {
+            System.out.println("INVALID DELIVERY DATE!!!");
+            throw new RuntimeException("Delivery date must be greater than pick up date.");
+        }
+        
+        // Notify manager that work item has been completed
+        manager.completeWorkItem(workItem.getId(), new HashMap<String,Object>());
+    }

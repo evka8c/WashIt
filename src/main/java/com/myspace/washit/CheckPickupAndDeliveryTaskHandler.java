fd0e7d3db8450fd90bd57cc9c436e091bc0f839e
user1 <>
2018-05-29 13:18:32 +0200

    {/src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java}

--- src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java
+++ src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java
@@ -29,4 +29,69 @@ public class CheckPickupAndDeliveryTaskHandler implements java.io.Serializable, 
     }
     
     public void	abortWorkItem(WorkItem workItem, WorkItemManager manager) {}
+    
+    
+    private static String streamToString(InputStream inputStream) 
+    {
+        String text = new Scanner(inputStream, "UTF-8").useDelimiter("\\Z").next();
+        return text;
+    }
+    
+    private static String jsonGetRequest(String url) 
+    {
+        String json = null;
+        try {
+            HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
+            
+            // Header
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+            // Send
+            con.setDoOutput(true);
+            con.setInstanceFollowRedirects(false);
+            con.connect();
+            InputStream inStream = con.getInputStream();
+            json = streamToString(inStream);
+            
+            int responseCode = con.getResponseCode();
+		    System.out.println("\nSending 'GET' request to URL : " + url);
+		    System.out.println("Response Code : " + responseCode);
+		    
+        }   catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        return json;
+    }
 }
+
+
+public void	executeWorkItem(WorkItem workItem, WorkItemManager manager) 
+    {
+        // Extract parameters
+        Customer customer = (Customer) workItem.getParameter("Customer");
+        
+        // Prepare url for email
+        String url = "https://washit-18577.firebaseio.com/customers.json?orderBy=\"email\"&equalTo=\"" + customer.getEmail() + "\"";
+        
+        // Check if customer exists
+        String json = jsonGetRequest(url);
+        if (json.contains(customer.getEmail())) 
+        {
+            System.out.println("EMAIL ALREADY EXISTS!!!");
+            throw new RuntimeException("There is already a customer with filled email address.");
+        };
+        
+        System.out.println("DATA OK!!!");
+        
+        // Notify manager that work item has been completed
+        manager.completeWorkItem(workItem.getId(), new HashMap<String,Object>());
+    }
+    
+    public void	abortWorkItem(WorkItem workItem, WorkItemManager manager) {}
+    
+    
+    

cc0ffaef1e7e4373790d319ffd5059f9277b3842
user1 <>
2018-05-29 19:12:09 +0200

    {/src/main/java/com/myspace/washit/MakePaymentTaskHandler.java}

--- src/main/java/com/myspace/washit/MakePaymentTaskHandler.java
+++ src/main/java/com/myspace/washit/MakePaymentTaskHandler.java
@@ -2,14 +2,14 @@ package com.myspace.washit;
 
 import com.myspace.washit.PaymentInfo;
 //import java.util.Map;
-//import java.util.HashMap;
-//import javax.json.Json;
-//import javax.json.JsonObject;
+import java.util.HashMap;
+import javax.json.Json;
+import javax.json.JsonObject;
 //import javax.json.JsonReader;
 //import java.net.URL;
 //import java.io.InputStreamReader;
 //import java.io.BufferedReader;
-//import java.io.StringReader;
+import java.io.StringReader;
 //import java.net.HttpURLConnection;
 
 import org.kie.api.runtime.process.WorkItem;

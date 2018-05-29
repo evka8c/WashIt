4073e88239a30f3ceba20e2cf548fafdfc8c087d
user1 <>
2018-05-29 13:28:04 +0200

    {/src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java}

--- src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java
+++ src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
 
 //import java.util.Random;
 import javax.json.Json;
-//import javax.json.JsonObject;
+import javax.json.JsonObject;
 import javax.json.JsonReader;
-//import java.net.URL;
+import java.net.URL;
 //import java.net.URLConnection;
 //import javax.net.ssl.HttpsURLConnection;
-//import java.io.InputStreamReader;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 //import java.io.DataOutputStream;
 //import java.io.StringReader;

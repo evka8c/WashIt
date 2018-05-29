db63ef905b8b4571e5cc08176cc9ac67e974c3cb
user1 <>
2018-05-29 12:53:53 +0200

    {/src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java}

--- src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java
+++ src/main/java/com/myspace/washit/CheckPickupAndDeliveryTaskHandler.java
@@ -18,8 +18,8 @@ public class CheckPickupAndDeliveryTaskHandler implements java.io.Serializable, 
     {
         // Extract parameters
         Order order = (Order) workItem.getParameter("Order");
-        Date pickupDate = order.getPickUpDate();
-        Date deliveryDate = order.getDeliveryDate();
+        Double pickupDate = order.getPickUpDate().getTime();
+        Double deliveryDate = order.getDeliveryDate().getTime();
         
         System.out.println("PICK UP DATE?: " + pickupDate);
         System.out.println("DELIVERY DATE?: " + deliveryDate);
@@ -31,47 +31,7 @@ public class CheckPickupAndDeliveryTaskHandler implements java.io.Serializable, 
             .build()
             .toString();
         
-        try {
-            HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
-            
-            // Header
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", "Mozilla/5.0");
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            
-            // Send
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		    wr.writeBytes(json);
-		    wr.flush();
-		    wr.close();
-		    
-		    int responseCode = con.getResponseCode();
-		    System.out.println("\nSending 'POST' request to URL : " + url);
-		    System.out.println("Post parameters : " + json);
-		    System.out.println("Response Code : " + responseCode);
-
-		    BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-	    	String inputLine;
-		    StringBuffer response = new StringBuffer();
-
-		    while ((inputLine = in.readLine()) != null) {
-			    response.append(inputLine);
-		    }
-		    in.close();
-		    
-		    // Store Firebase Id
-		    JsonReader jsonReader = Json.createReader(new StringReader(response.toString()));
-            JsonObject object = jsonReader.readObject();
-            jsonReader.close();
-            String firebaseId = object.getString("name");
-            customer.setFirebaseId(firebaseId);
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        
 
         System.out.println("CUSTOMER CREATED!!!");
         

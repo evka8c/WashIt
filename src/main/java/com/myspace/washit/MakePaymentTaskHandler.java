8079a1ab56c0e404e8382b4e8bd10f9cf9c720da
user1 <>
2018-05-29 22:52:43 +0200

    {/src/main/java/com/myspace/washit/MakePaymentTaskHandler.java}

--- src/main/java/com/myspace/washit/MakePaymentTaskHandler.java
+++ src/main/java/com/myspace/washit/MakePaymentTaskHandler.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 
 import org.kie.api.runtime.process.WorkItem;

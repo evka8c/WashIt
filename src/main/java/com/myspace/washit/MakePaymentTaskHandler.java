2f391340e25581f624cf2368fba68ffc98b33229
user1 <>
2018-05-29 19:12:25 +0200

    {/src/main/java/com/myspace/washit/MakePaymentTaskHandler.java}

--- src/main/java/com/myspace/washit/MakePaymentTaskHandler.java
+++ src/main/java/com/myspace/washit/MakePaymentTaskHandler.java
@@ -5,10 +5,10 @@ import com.myspace.washit.PaymentInfo;
 import java.util.HashMap;
 import javax.json.Json;
 import javax.json.JsonObject;
-//import javax.json.JsonReader;
-//import java.net.URL;
+import javax.json.JsonReader;
+import java.net.URL;
 //import java.io.InputStreamReader;
-//import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.StringReader;
 //import java.net.HttpURLConnection;
 
